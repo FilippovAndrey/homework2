8ce92bf3ed5d3dd8f28546ff5488529ecacb0112
Andrey Filippov <Viper4478@gmail.com>
2021-10-22 17:21:11 +0300

    add semaphore

--- src/main/java/ru/innotech/education/rxjava/reader.writer/ReadWriteList.java
+++ src/main/java/ru/innotech/education/rxjava/reader.writer/ReadWriteList.java
@@ -7,6 +7,7 @@ import net.sf.cglib.proxy.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.*;
+import java.util.concurrent.Semaphore;
 
 public class ReadWriteList<T>
         implements Collection<T>, IList {
@@ -18,26 +19,34 @@ public class ReadWriteList<T>
                 ReadWriteList.class.getInterfaces(),
                 new AnnotationHandler(ReadWriteList.class));
         return (ReadWriteList<T>) rwl;*/
+        Semaphore semaphore = new Semaphore(1);
+
         ReadWriteList<T> list = new ReadWriteList<>();
         Enhancer enhancer = new Enhancer();
         enhancer.setSuperclass(list.getClass());
         enhancer.setCallback(new InvocationHandler() {
             @Override
-            public synchronized Object invoke(Object proxy, Method method, Object[] args)
+            public Object invoke(Object proxy, Method method, Object[] args)
                     throws Throwable {
+                Object invoke = null;
                 Method[] methods = ReadWriteList.class.getClass().getDeclaredMethods();
                 Method method1 = Arrays.stream(methods).filter(t -> t.getName().equals(method.getName())).findFirst().orElse(method);
                 //System.out.println("method 1: " + method1);
                 //if(Arrays.stream(methods).findFirst().filter(t -> t.getName().equals(method.getName())).isPresent()) {
                 //System.out.println("method in invoke: " + method.getName());
 //      if (!method1.getName().equals(method.getName())) break;
-                if (method1.getDeclaredAnnotation(ReadOperation.class) != null) { // TODO READ OPERATION LOGIC
+                if (method1.getDeclaredAnnotation(ReadOperation.class) != null) { // TODO READ OPERATION LOGIC – несколько потоков могут выполнять этот метод параллельно.
                     System.out.println("ReadOperataion - " + method.getName());
+
                 }
-                if (method1.getDeclaredAnnotation(WriteOperation.class) != null) { // TODO WRITE OPERATION LOGIC
+                if (method1.getDeclaredAnnotation(WriteOperation.class) != null) { // TODO WRITE OPERATION LOGIC – только один поток в один момент времени может выполнять этот метод.
                     System.out.println("WriteOperataion - " + method.getName());
+                    semaphore.acquire();
+                    invoke = method.invoke(list, args);
+                    semaphore.release();
                 }
-                return method.invoke(list, args);
+
+                return invoke==null?method.invoke(list, args):invoke;
             }
         });
         return (ReadWriteList<T>) enhancer.create();
