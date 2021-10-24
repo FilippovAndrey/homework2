138b2ce492118f3baa7fadc12f6b53cb48e25677
Andrey Filippov <viper4478@gmail.com>
2021-10-25 01:02:45 +0300

    refactoring code

--- src/main/java/ru/innotech/education/rxjava/reader.writer/ReadWriteList.java
+++ src/main/java/ru/innotech/education/rxjava/reader.writer/ReadWriteList.java
@@ -1,53 +1,43 @@
 package ru.innotech.education.rxjava.reader.writer;
 
 import net.sf.cglib.proxy.Enhancer;
-import net.sf.cglib.proxy.FixedValue;
 import net.sf.cglib.proxy.InvocationHandler;
 
 import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
 import java.util.*;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ReadWriteList<T>
         implements Collection<T> {
     private final List<T> list = new ArrayList<>();
 
     public static <T> ReadWriteList<T> create() {
-        /*IList rwl = (IList) Proxy.newProxyInstance(
-                ReadWriteList.class.getClassLoader(),
-                ReadWriteList.class.getInterfaces(),
-                new AnnotationHandler(ReadWriteList.class));
-        return (ReadWriteList<T>) rwl;*/
-        Semaphore semaphore = new Semaphore(1);
 
-        ReadWriteList<T> list = new ReadWriteList<>();
-        Enhancer enhancer = new Enhancer();
+        final Semaphore semaphore = new Semaphore(1);
+        final AtomicInteger countReader = new AtomicInteger(0);
+
+        final ReadWriteList<T> list = new ReadWriteList<>();
+        final Enhancer enhancer = new Enhancer();
         enhancer.setSuperclass(list.getClass());
-        enhancer.setCallback(new InvocationHandler() {
-            @Override
-            public Object invoke(Object proxy, Method method, Object[] args)
-                    throws Throwable {
-                Object invoke = null;
-                Method[] methods = ReadWriteList.class.getClass().getDeclaredMethods();
-                Method method1 = Arrays.stream(methods).filter(t -> t.getName().equals(method.getName())).findFirst().orElse(method);
-                //System.out.println("method 1: " + method1);
-                //if(Arrays.stream(methods).findFirst().filter(t -> t.getName().equals(method.getName())).isPresent()) {
-                //System.out.println("method in invoke: " + method.getName());
-//      if (!method1.getName().equals(method.getName())) break;
-                if (method1.getDeclaredAnnotation(ReadOperation.class) != null) { // TODO READ OPERATION LOGIC – несколько потоков могут выполнять этот метод параллельно.
-                    System.out.println("ReadOperataion - " + method.getName());
+        enhancer.setCallback((InvocationHandler) (proxy, method, args) -> {
+            Object invoke = null;
+            final Method[] methods = ReadWriteList.class.getDeclaredMethods();
+            final Method originMethod = Arrays.stream(methods)
+                    .filter(t -> t.getName().equals(method.getName()))
+                    .findFirst()
+                    .orElse(method);
 
-                }
-                if (method1.getDeclaredAnnotation(WriteOperation.class) != null) { // TODO WRITE OPERATION LOGIC – только один поток в один момент времени может выполнять этот метод.
-                    System.out.println("WriteOperataion - " + method.getName());
-                    semaphore.acquire();
-                    invoke = method.invoke(list, args);
-                    semaphore.release();
-                }
-
-                return invoke == null ? method.invoke(list, args) : invoke;
+            if (originMethod.getDeclaredAnnotation(ReadOperation.class) != null) { // – несколько потоков могут выполнять этот метод параллельно.
+                System.out.println("ReadOperataion - " + method.getName());
             }
+            if (originMethod.getDeclaredAnnotation(WriteOperation.class) != null) { // – только один поток в один момент времени может выполнять этот метод.
+                semaphore.acquire();
+                invoke = method.invoke(list, args);
+                semaphore.release();
+            }
+
+            return invoke == null ? method.invoke(list, args) : invoke;
         });
         return (ReadWriteList<T>) enhancer.create();
     }
